baa158b4b578d39c63bc4abb613fe6224a3773af
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:45:33 +0200

    Add files via upload

---
+++ Advanced Palindromechecker/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Skriv en mening : ");
+
+string input = Console.ReadLine();
+
+string cleanedInput = new string(input.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+
+char[] normalString = cleanedInput.ToCharArray();
+
+
+Array.Reverse(normalString);
+
+
+
+string reversed = new string(normalString);
+
+if(cleanedInput == reversed)
+{
+    Console.WriteLine("Palindrom");
+}
+else
+{
+    Console.WriteLine("Inte en palindrom");
+}
